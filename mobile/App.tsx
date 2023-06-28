64d37db1b89a9d265f975843a9fed996ab1bff58
Pietro <pietro.developer@gmail.com>
2023-06-28 17:55:08 -0300

    feat(mobile): add app wide container

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,78 +1,82 @@
-import { useCallback, useEffect, useState } from "react";
-import { View } from "react-native";
+import { useCallback, useEffect, useState } from "react"
 
-import { StatusBar } from "expo-status-bar";
-import * as SplashScreen from "expo-splash-screen";
-import { loadAsync } from "expo-font";
+import { StatusBar } from "expo-status-bar"
+import * as SplashScreen from "expo-splash-screen"
+import { loadAsync } from "expo-font"
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
-import { appTheme } from "./src/theme/app-theme";
-import { navigatorScreenOptions } from "./src/theme/screen-options";
-import { styles } from "./style";
+import { appTheme } from "./src/theme/app-theme"
+import { navigatorScreenOptions } from "./src/theme/screen-options"
 
-import { OnboardingScreen } from "./src/screens/OnboardingScreen/OnboardingScreen";
-import { RootStackParamList } from "./src/types/navigator-types";
-import { LoginScreen } from "./src/screens/LoginScreen/LoginScreen";
-import { ForgotPasswordScreen } from "./src/screens/ForgotPasswordScreen/ForgotPasswordScreen";
-import { RegisterNewAccountScreen } from "./src/screens/RegisterNewAccountScreen/RegisterNewAccountScreen";
-import { NgoLoginScreen } from "./src/screens/NgoLoginScreen/NgoLoginScreen";
+import { OnboardingScreen } from "./src/screens/OnboardingScreen/OnboardingScreen"
+import { RootStackParamList } from "./src/types/navigator-types"
+import { LoginScreen } from "./src/screens/LoginScreen/LoginScreen"
+import { ForgotPasswordScreen } from "./src/screens/ForgotPasswordScreen/ForgotPasswordScreen"
+import { RegisterNewAccountScreen } from "./src/screens/RegisterNewAccountScreen"
+import { NgoLoginScreen } from "./src/screens/NgoLoginScreen/NgoLoginScreen"
+import { SafeAreaProvider } from "react-native-safe-area-context"
+import { AppContainer } from "./src/core/components/AppContainer"
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createNativeStackNavigator<RootStackParamList>()
 
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
-  const [appIsReady, setAppIsReady] = useState<boolean>(false);
+  const [appIsReady, setAppIsReady] = useState<boolean>(false)
 
   useEffect(() => {
     const loadFonts = async () => {
       try {
         await loadAsync({
           Poppins: require("./assets/fonts/Poppins.ttf"),
-        });
+        })
       } catch (error) {
-        console.error(error);
+        console.error(error)
       } finally {
-        setAppIsReady(true);
+        setAppIsReady(true)
       }
-    };
+    }
 
-    loadFonts();
-  }, []);
+    loadFonts()
+  }, [])
 
   const onLayoutRootView = useCallback(async () => {
     if (appIsReady) {
-      await SplashScreen.hideAsync();
+      await SplashScreen.hideAsync()
     }
-  }, [appIsReady]);
+  }, [appIsReady])
 
   if (!appIsReady) {
-    return null;
+    return null
   }
 
   return (
-    <View style={styles.appContainer} onLayout={onLayoutRootView}>
+    <>
       <StatusBar style="dark" />
-      <NavigationContainer theme={appTheme}>
-        <Stack.Navigator
-          screenOptions={navigatorScreenOptions}
-          initialRouteName="Onboarding"
-        >
-          <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen
-            name="ForgotPassword"
-            component={ForgotPasswordScreen}
-          />
-          <Stack.Screen
-            name="RegisterNewAccount"
-            component={RegisterNewAccountScreen}
-          />
-          <Stack.Screen name="NgoLogin" component={NgoLoginScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
-  );
+      <SafeAreaProvider>
+        <AppContainer onLayout={onLayoutRootView}>
+          <NavigationContainer theme={appTheme}>
+            <Stack.Navigator
+              screenOptions={navigatorScreenOptions}
+              initialRouteName="Onboarding"
+            >
+              <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen
+                name="ForgotPassword"
+                component={ForgotPasswordScreen}
+              />
+              <Stack.Screen
+                name="RegisterNewAccount"
+                component={RegisterNewAccountScreen}
+              />
+              <Stack.Screen name="NgoLogin" component={NgoLoginScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </AppContainer>
+      </SafeAreaProvider>
+    </>
+  )
 }
